99f5c04cd52ec63bfaf1142520ff606b88d3b729
LuisAlejandroEspinisa <espinosaluis18@gmail.com>
2023-06-06 18:34:14 -0500

    modulo 01

---
+++ mi primier proyecto de node/Untitled-1.js
@@ -0,0 +1,20 @@
+// tipos primitivos
+//number
+4;
+// string
+"hola mundo";
+`hola mundo`;
+
+// booleanos
+true;
+false;
+
+// null y undefinet
+null;
+undefined;
+//null,undefined, false,0
+if (undefined){
+console.log("cumple")}
+else{
+   console.log("no cumple");
+}
